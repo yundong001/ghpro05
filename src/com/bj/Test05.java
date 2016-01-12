eba0e81b00a6bbbf76c3ef2bf5125b6f044e2469
lwch01 <suhua22287@sina.com>
2016-01-12 10:47:17 +0800

    yy

--- src/com/bj/Test05.java
+++ src/com/bj/Test05.java
@@ -3,7 +3,7 @@ package com.bj;
 public class Test05 {
 
 	public static void main(String[] args) {
-		System.out.println(56);
+		System.out.println(78);
 		System.out.println("master merge in dev01 in dev01");
 
 	}
